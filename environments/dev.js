fa2ea793859751e42b25cf6e971641d4fce8623c
Rafael Camargo <contato@rafaelcamargo.com>
2017-05-19 01:26:12 -0300

    Including Google Analytics

--- environments/dev.js
+++ environments/dev.js
@@ -6,7 +6,8 @@
       url: 'https://sheetsu.com/apis/v1.0/f343f211c06b',
       key: 'mfNDqbyXCmqc1zANtrWX',
       secret: 'zVh3i4YghzEuP9CeDSk66BX1pqZiakbAyhHip2Bc'
-    }
+    },
+    googleAnalyticsId: 'UA-99548032-2'
   }
 
 }());
