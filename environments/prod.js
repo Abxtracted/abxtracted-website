a5fea8f8910a90475733b79dbc29f9120b7ea7ac
Rafael Camargo <contato@rafaelcamargo.com>
2018-12-16 19:55:12 -0200

    Remove prod keys

--- environments/prod.js
+++ environments/prod.js
@@ -3,13 +3,13 @@
   window.environment = {
     appBaseUrl: 'https://app.abxtracted.com',
     sheetsu: {
-      url: 'https://sheetsu.com/apis/v1.0/f343f211c06b',
-      key: 'mfNDqbyXCmqc1zANtrWX',
-      secret: 'zVh3i4YghzEuP9CeDSk66BX1pqZiakbAyhHip2Bc'
+      url: '',
+      key: '',
+      secret: ''
     },
-    googleAnalyticsId: 'UA-99548032-1',
-    mixPanelToken: 'f8ceded21b8d80f7de4be6899f24dbe9',
-    abxtractedProjectId: '8fec21c0-7dc8-4a0d-8e86-4ba35b9e2f36'
+    googleAnalyticsId: '',
+    mixPanelToken: '',
+    abxtractedProjectId: ''
   };
 
 }());
